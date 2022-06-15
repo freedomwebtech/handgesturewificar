7e088b9e27a98129680eee43e5a20e341295e456
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2022-06-15 07:39:37 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,3 +1,4 @@
+
 from umqtt.simple import MQTTClient
 import machine
 from time import sleep
@@ -12,17 +13,30 @@ p2 = machine.PWM(machine.Pin(12), freq=10000)
 p3 = machine.PWM(machine.Pin(13), freq=10000)
 led = machine.Pin(16, machine.Pin.OUT)
 
+
 def msg(a,b):
     data=(str(b,'utf-8'))
     print(data)
     if "one" in data:
-        for i in range(400,600):
+        for i in range(600):
             p.duty(0)
             p1.duty(i)
+            p2.duty(i)
+            p3.duty(0)
+    if "two" in data:
+        for j in range(600):
+            p.duty(j)
+            p1.duty(0)
             p2.duty(0)
-            p3.duty(i)
-  
-  
+            p3.duty(j)
+    if "three" in data:
+        for j in range(900):
+            p.duty(0)
+            p1.duty(j)
+            p2.duty(0)
+            p3.duty(j)
+           
+   
     if "goodbye" in data:
             p.duty(0)
             p1.duty(0)
@@ -30,13 +44,6 @@ def msg(a,b):
             p3.duty(0)
         
                     
-    
-    if "oner" in data:
-        for m in range(400,600):
-            p.duty(m)
-            p1.duty(0)
-            p2.duty(m)
-            p3.duty(0)
     
    
                     
